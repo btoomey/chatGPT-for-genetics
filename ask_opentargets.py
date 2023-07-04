e626a62d5afa2d2f8a1a1c14a9e28aa385f03ba2
Bridget Toomey <bridget@lumindx.com>
2023-07-03 22:08:24 -0600

    Generation of single-step GraphQL queries is mostly working. But I still have to subset results properly to get desired outputs and solve two-step queries.

--- ask_opentargets.py
+++ ask_opentargets.py
@@ -11,8 +11,17 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 # Open Targets graphQL schema example
 # read from file
-with open("graphql_schema.txt", "r") as f:
-    prompt_template = f.read()
+with open("graphql_query_apoe.txt", "r") as f:
+    prompt_template_apoe = f.read()
+
+with open("graphql_query_ulcerative_colitis.txt", "r") as f:
+    prompt_template_ulcerative_colitis = f.read()
+
+with open("graphql_query_vorinostat.txt", "r") as f:
+    prompt_template_vorinostat = f.read()
+
+with open("open_targets_gql_schema.txt", "r") as f:
+    open_targets_schema = f.read()
 
 # Prime the target query for completion
 prime_prompt = "query top_n_associated_diseases {\n  search(queryString:"
@@ -21,9 +30,68 @@ prime_prompt = "query top_n_associated_diseases {\n  search(queryString:"
 # user_input = "Find the top 2 diseases associated with BRCA1"
 user_input = input("How can I help you today?\n")
 
-response = openai.Completion.create(
-    model="code-davinci-002",
-    prompt=prompt_template + "### " + user_input + "\n" + prime_prompt,
+# To keep my costs low as I develop this application, I'm going to use 
+# gpt-3.5-turbo instead of the older Da Vinci models.
+# The downside of this model is that it requires more prompting/examples.
+# (See https://stackoverflow.com/questions/76192496/openai-v1-completions-vs-v1-chat-completions-end-points)
+# TO-DO: Provide examples conditional on type of input text
+messages = [
+    {
+        "role": "system",
+        "content": f"Here is the schema of the Open Targets Platform GraphQL endpoint: {open_targets_schema}"
+    },
+    {
+        "role": "system",
+        "content": "You are generating GraphQL queries to the Open Targets Platform endpoint. Only return the query itself. Do not describe the query or summarize it in any way."
+    },
+    {
+        "role": "system",
+        "content": "If the user ever asks a question with the same meaning as one of the previous example questions, provide the corresponding example answer."
+    },
+    {
+        "role": "system",
+        "name": "example_user",
+        "content": "What are the top 5 diseases associated with APOE?"
+    },
+    {
+        "role": "system",
+        "name": "example_assistant",
+        "content": prompt_template_apoe
+    },
+    {
+        "role": "system",
+        "name": "example_user",
+        "content": "What are the top drugs that can treat ulcerative colitis?"
+    },
+    {
+        "role": "system",
+        "name": "example_assistant",
+        "content": prompt_template_ulcerative_colitis
+    },
+    {
+        "role": "system",
+        "name": "example_user",
+        "content": "What are the main targets of vorinostat?"
+    },
+    {
+        "role": "system",
+        "name": "example_assistant",
+        "content": prompt_template_vorinostat
+    },
+    {
+        "role": "user",
+        "content": user_input
+    },
+    {
+        "role": "assistant",
+        "content": "query "
+    }
+]
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-16k",
+    messages=messages,
+    # prompt=prompt_template + "### " + user_input + "\n" + prime_prompt,
     temperature=0,
     max_tokens=250,
     top_p=1,
@@ -31,36 +99,49 @@ response = openai.Completion.create(
     presence_penalty=0,
     stop=["###"],
 )
-response_text = response["choices"][0].text
+response_text = response['choices'][0]['message']['content']
 
-query_string = prime_prompt + response_text
+# response = openai.Completion.create(
+#     model="gpt-3.5-turbo",
+#     prompt=prompt_template + "### " + user_input + "\n" + prime_prompt,
+#     temperature=0,
+#     max_tokens=250,
+#     top_p=1,
+#     frequency_penalty=0,
+#     presence_penalty=0,
+#     stop=["###"],
+# )
+# response_text = response["choices"][0].text
+print(f'response_text: {response_text}', flush=True)
 
-# filename with current date and time
-query_file = "query_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt"
+# query_string = prime_prompt + response_text
 
-# write query to file with current date and time
-with open(query_file, "w") as f:
-    f.write(f"# User input: {user_input}\n")
-    f.write(query_string)
-    print(f"\nCustom graphQL query was written to file: {query_file}")
+# # filename with current date and time
+# query_file = "query_" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".txt"
 
-# Set base URL of GraphQL API endpoint
-base_url = "https://api.platform.opentargets.org/api/v4/graphql"
+# # write query to file with current date and time
+# with open(query_file, "w") as f:
+#     f.write(f"# User input: {user_input}\n")
+#     f.write(query_string)
+#     print(f"\nCustom graphQL query was written to file: {query_file}")
 
-# Perform POST request and check status code of response
-# This handles the cases where the Open Targets API is down or our query is invalid
-try:
-    response = requests.post(base_url, json={"query": query_string})
-    response.raise_for_status()
-except requests.exceptions.HTTPError as err:
-    print(err)
+# # Set base URL of GraphQL API endpoint
+# base_url = "https://api.platform.opentargets.org/api/v4/graphql"
 
-# Transform API response from JSON into Python dictionary and print in console
-api_response = json.loads(response.text)
-hits_list = api_response["data"]["search"]["hits"][0]
+# # Perform POST request and check status code of response
+# # This handles the cases where the Open Targets API is down or our query is invalid
+# try:
+#     response = requests.post(base_url, json={"query": query_string})
+#     response.raise_for_status()
+# except requests.exceptions.HTTPError as err:
+#     print(err)
 
-print("\n\nQuerying Open Targets genetics database...\n\n")
+# # Transform API response from JSON into Python dictionary and print in console
+# api_response = json.loads(response.text)
+# hits_list = api_response["data"]["search"]["hits"][0]
 
-disease_list = extract_values(hits_list, "disease")
-for i, j in enumerate(disease_list):
-    print(f"{i+1}. {j['name']}")
+# print("\n\nQuerying Open Targets genetics database...\n\n")
+
+# disease_list = extract_values(hits_list, "disease")
+# for i, j in enumerate(disease_list):
+#     print(f"{i+1}. {j['name']}")
